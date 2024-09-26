219d3e5b7d83ac8a7d2d720bcfe5047c10b6a9b0
NITIN JHA <115605662+nitinjha607@users.noreply.github.com>
2024-09-26 21:43:41 +0530

    Create Two_sum.py

---
+++ Two_sum.py
@@ -0,0 +1,14 @@
+# Two Sum
+# Solution-1 Easy and optimized solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap ={} # val : index
+        for i,n in enumerate(nums):
+            diff = target -n
+            if diff in prevMap:
+                return [prevMap[diff],i]
+            prevMap[n]=i
+        return    
+
+
+        
